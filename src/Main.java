ef8b2ca5507b628d34b9bf2a47704c78fe24a0cc
jona.boecker <jona.boecker@web.de>
2024-03-18 17:37:54 +0100

    intellij export as jar

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import aqua.blatt1.broker.Broker;
 import aqua.blatt1.client.Aqualife;
 
 public class Main {
     public static void main(String[] args) {
-        Aqualife.main(args);
-        Aqualife.main(args);
+            Aqualife.main(args);
     }
 }
